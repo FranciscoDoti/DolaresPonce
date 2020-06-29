81dee64655dd5fb8e6c2c8bcc4a8ceb2dcd53c56
unknown <franciscodotitexeira@gmail.com>
2020-06-29 11:10:57 -0300

    funca mejor ahora con los argv y el switch

--- precioDai.js
+++ precioDai.js
@@ -4,46 +4,46 @@ const argv = require('minimist')(process.argv.slice(2));
 
 
 var options = {
-  'method': 'GET',
-  'url': 'https://criptoya.com/api/buenbit/dai/ars',
-  'headers': {
-    
-  }
-};
+    'method': 'GET',
+    'url': 'https://criptoya.com/api/buenbit/dai/ars',
+    'headers': {
 
-var buenBit= {
+    }
+}
+
+var buenBit = {
     ARS: {
         compra: '',
         venta: ''
     },
     USD: {
         compra: '',
-        venta:''
+        venta: ''
     }
-};
+}
 
 
+request(options, async function (error, response) {
+    if (error) throw new Error(error);
+    var resp = await JSON.parse(response.body);
+    buenBit.ARS.compra = await parseFloat(resp.totalAsk);
+    buenBit.ARS.venta = await parseFloat(resp.totalBid);
+    await console.log("BuenBit ARS precio Compra Total: " + buenBit.ARS.compra);
+    await console.log("BuenBit ARS precio Venta Total " + buenBit.ARS.venta);
 
 
- request(options, function (error, response) {
-  if (error) throw new Error(error);
-  var resp = JSON.parse(response.body);
-  buenBit.ARS.compra =parseFloat( resp.totalAsk);
-  buenBit.ARS.venta = parseFloat(resp.totalBid);
-  
-  console.log("BuenBit ARS precio Compra Total: " + buenBit.ARS.compra);
-  console.log("BuenBit ARS precio Venta Total " + buenBit.ARS.venta);
-
-  switch(argv.tipoOperacion){
-    case 'COMPRA' : {
-        expect(buenBit.ARS.compra).to.be.below(argv.montoBarrera);
-        break;
-      };
-    case 'VENTA' : {
-        expect(buenBit.ARS.venta).to.be.greaterThan(argv.montoBarrera);
-        break;
-  }
-
+    switch (argv.tipoOperacion) {
+        case 'COMPRA': {
+            await expect(buenBit.ARS.compra, "El valor para la compra no está debajo del valor esperado").to.be.below(argv.montoBarrera);
+            break;
+        };
+        case 'VENTA': {
+            await expect(buenBit.ARS.venta,  "El valor para la venta no está por encima del valor esperado").to.be.greaterThan(argv.montoBarrera);
+            break;
+        }
+    }
 
     }
-});
+)
+
+
