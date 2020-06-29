4f8413721728562b851e7dc5d333ab3eb6e582c2
unknown <franciscodotitexeira@gmail.com>
2020-06-29 00:10:13 -0300

    precioDai

---
+++ precioDai.js
@@ -0,0 +1,38 @@
+var request = require('request');
+const { assert, expect } = require('chai');
+
+var options = {
+  'method': 'GET',
+  'url': 'https://criptoya.com/api/buenbit/dai/ars',
+  'headers': {
+    
+  }
+};
+
+var buenBit= {
+    ARS: {
+        compra: '',
+        venta: ''
+    },
+    USD: {
+        compra: '',
+        venta:''
+    }
+};
+
+
+
+ request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+  var resp = JSON.parse(response.body);
+  buenBit.ARS.compra =parseFloat( resp.totalAsk);
+  buenBit.ARS.venta = parseFloat(resp.totalBid);
+  
+  console.log("BuenBit ARS precio Compra Total: " + buenBit.ARS.compra);
+  console.log("BuenBit ARS precio Venta Total " + buenBit.ARS.venta);
+
+  expect(buenBit.ARS.compra).to.be.below(125);
+//  expect(buenBit.ARS.venta).to.be.greaterThan(124);
+ });
+
